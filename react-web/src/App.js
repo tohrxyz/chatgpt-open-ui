6e9738e9f5a3a5051053f7c7756078c3f79a7aa2
Tomáš Hrib <103599310+tohrxyz@users.noreply.github.com>
2023-03-06 17:29:38 +0100

    Add hash spinner for loading (#4)

--- react-web/src/App.js
+++ react-web/src/App.js
@@ -1,24 +1,35 @@
 import './App.css';
 import { useState } from 'react'
 import onSubmit from './api/api';
+import HashLoader from 'react-spinners/HashLoader';
 
 function App() {
   const [apiKey, setApiKey] = useState("");
   const [inputText, setInputText] = useState("");
   const [result, setResult] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    
-    onSubmit(apiKey, inputText, setResult);
+    setLoading(true);
+    onSubmit(apiKey, inputText, setResult).then(() => {
+      setLoading(false);
+    });
   }
+
+  // overrides default css for HashLoader spinner
+  const override = {
+    display: "block",
+    margin: "0 auto",
+  }
+
   return (
     <div className="container mx-auto py-4 max-w-3xl">
+
       <h1 className="text-4xl font-bold mb-4 text-center">ChatGPT Open UI</h1>
+
       <form onSubmit={handleSubmit} className="flex flex-col mx-5">
-        <label htmlFor="api-key" className="mb-2 font-bold">
-          API Key:
-        </label>
+        <label htmlFor="api-key" className="mb-2 font-bold">API Key:</label>
         <input
           type="text"
           id="api-key"
@@ -26,25 +37,38 @@ function App() {
           onChange={(e) => setApiKey(e.target.value)}
           className="border border-gray-400 p-2 mb-4"
         />
-        <label htmlFor="input-text" className="mb-2 font-bold">
-          Input Text:
-        </label>
+
+        <label htmlFor="input-text" className="mb-2 font-bold">Input Text:</label>
         <textarea
           id="input-text"
           value={inputText}
           onChange={(e) => setInputText(e.target.value)}
           className="border border-gray-400 p-2 mb-4"
         ></textarea>
-        <button type="submit" className="bg-blue-500 text-white py-2 px-4 rounded">
-          Submit
-        </button>
+
+        <div className="flex justify-center">
+          <button type="submit" className="bg-blue-500 text-white py-3 px-4 rounded text-center w-60 flex justify-center" disabled={loading}>
+            {loading ? 
+              <HashLoader 
+                color="white"
+                loading={loading}
+                size={25}
+                aria-label="Loading"
+                cssOverride={override}
+              /> : <div>Submit</div>
+            }
+          </button>
+        </div>
       </form>
+      
       {result && (
         <div className="border border-gray-400 p-4 mt-4 mx-4">
           <h2 className="font-bold mb-2">Result:</h2>
           <p>{result}</p>
         </div>
       )}
+
+      
     </div>
   );
 }
