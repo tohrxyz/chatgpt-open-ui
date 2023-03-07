239c29e5b8eeb7475651431e82d19ff4244850e0
Tomáš Hrib <103599310+tohrxyz@users.noreply.github.com>
2023-03-07 20:27:32 +0100

    Add copy to clipboard button (#12)
    * Add button to copy
    
    * Fix layout of language and copy button
    
    * Change to 'Copied' and then reset after click

--- react-web/src/components/Codeblock.js
+++ react-web/src/components/Codeblock.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Prism from "../prism/prism.js";
 import '../prism/prism.css';
 
@@ -7,9 +7,24 @@ export default function Codeblock(props) {
     Prism.highlightAll();
   }, []);
 
+  const [copied, setCopied] = useState(false);
+
+  const handleClick = () => {
+    setCopied(true);
+    console.log(copied);
+    navigator.clipboard.writeText(props.children);
+    setTimeout(() => {
+      setCopied(false);
+      console.log(copied);
+    }, 3000);
+  }
+
   return (
     <pre>
-        <div className='text-sm py-1 px-4 -m-4 mb-4 bg-gray-700 text-gray-300 rounded-sm'>{props.language}</div>
+        <div className='flex justify-between text-sm py-1 px-4 -m-4 mb-4 bg-gray-700 text-gray-300 rounded-sm'>
+          <div>{props.language}</div>
+          <button onClick={handleClick}>{copied ? 'Copied' : 'Copy'}</button>
+        </div>
         <code className={`language-${props.language}`}>
             {props.children}
         </code>
