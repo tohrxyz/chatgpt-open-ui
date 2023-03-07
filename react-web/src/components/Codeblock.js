e9783402be28a0e5167d594fad72907aefdde807
tomashrib <tomas.hrib@proton.me>
2023-03-07 19:17:20 +0100

    Add button to copy

--- react-web/src/components/Codeblock.js
+++ react-web/src/components/Codeblock.js
@@ -7,9 +7,20 @@ export default function Codeblock(props) {
     Prism.highlightAll();
   }, []);
 
+  const handleClick = () => {
+    navigator.clipboard.writeText(props.children);
+  }
+
   return (
     <pre>
-        <div className='text-sm py-1 px-4 -m-4 mb-4 bg-gray-700 text-gray-300 rounded-sm'>{props.language}</div>
+        <div 
+          className='text-sm py-1 px-4 -m-4 mb-4 bg-gray-700 text-gray-300 rounded-sm flex flex-row justify-between'
+        >
+          <div>{props.language}</div>
+          <button onClick={handleClick}>
+            Copy
+          </button>
+        </div>
         <code className={`language-${props.language}`}>
             {props.children}
         </code>
