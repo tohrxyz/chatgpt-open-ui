e32f82d71169a16c55f7cc57d374fbb1e5c5ea5b
tomashrib <tomas.hrib@proton.me>
2023-03-07 20:15:15 +0100

    Fix layout of language and copy button

--- react-web/src/components/Codeblock.js
+++ react-web/src/components/Codeblock.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Prism from "../prism/prism.js";
 import '../prism/prism.css';
 
@@ -13,13 +13,9 @@ export default function Codeblock(props) {
 
   return (
     <pre>
-        <div 
-          className='text-sm py-1 px-4 -m-4 mb-4 bg-gray-700 text-gray-300 rounded-sm flex flex-row justify-between'
-        >
+        <div className='flex justify-between text-sm py-1 px-4 -m-4 mb-4 bg-gray-700 text-gray-300 rounded-sm'>
           <div>{props.language}</div>
-          <button onClick={handleClick}>
-            Copy
-          </button>
+          <button onClick={handleClick}>Copy</button>
         </div>
         <code className={`language-${props.language}`}>
             {props.children}
