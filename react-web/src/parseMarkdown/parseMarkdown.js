a8061ae7a0f80c367a70dad284c78edf85019c1f
Tomáš Hrib <103599310+tohrxyz@users.noreply.github.com>
2023-03-08 22:44:06 +0100

    Fix blank site after failed parsing + code highlighting (#26)
    * Add null-assertion to language and code extraction from string
    
    * Fix model not specifying language after markdown ```
    
    * Correct language names for Prism.js highlighter

--- react-web/src/parseMarkdown/parseMarkdown.js
+++ react-web/src/parseMarkdown/parseMarkdown.js
@@ -16,11 +16,11 @@ export default function parseMarkdown(markdown) {
       if (part.startsWith('```')) {
         // extracts language and code from the string codeblock
         let matches = part.match(/^```(\S*)\n([\s\S]*)\n```$/);
-        let language = matches[1];
-        let code = matches[2];
+        let language = matches?.[1] ?? '';
+        let code = matches?.[2] ?? '';
 
         return (
-            <Codeblock key={index} language={language}>
+            <Codeblock key={index} language={correctLanguage(language)}>
               {code.trim()}
             </Codeblock>
         );
@@ -37,4 +37,19 @@ export default function parseMarkdown(markdown) {
 
   // if no parsing is needed, returns original string
   return markdown;
+}
+
+// converts language names to the ones used by Prism.js
+function correctLanguage(language) {
+  
+  // convert language to lowercase
+  language = language.toLowerCase();
+  
+  switch(language) {
+    case "vue": return "js";
+    case "react": return "js";
+    case "c++": return "cpp";
+    case "c#": return "cs";
+    default: return language;
+  }
 }
