2b3831dc87e5b03be93211dad4ac6faecfbc9fe0
mariojaros <majo.jaros@gmail.com>
2023-03-12 16:49:08 +0100

    Prompt input field height isn't dynamic (#15)

---
+++ react-web/src/dynamicInput/dynamicInput.js
@@ -0,0 +1,13 @@
+
+
+function getTextareaRowsCount(textarea) {
+    const styles = window.getComputedStyle(textarea);
+    const lineHeight = parseInt(styles.lineHeight);
+    const borderSize = parseInt(styles.borderTopWidth) + parseInt(styles.borderBottomWidth);
+    const paddingSize = parseInt(styles.paddingTop) + parseInt(styles.paddingBottom);
+    const contentHeight = textarea.scrollHeight - paddingSize - borderSize;
+    const rowsCount = Math.ceil(contentHeight / lineHeight);
+    return rowsCount;
+}
+
+export default getTextareaRowsCount;
