62bbfb649581400069806b2629d954b0b7e02c11
tomashrib <tomas.hrib@proton.me>
2023-03-13 08:41:13 +0100

    Initial try to decrease row count

--- react-web/src/dynamicInput/dynamicInput.js
+++ react-web/src/dynamicInput/dynamicInput.js
@@ -1,10 +1,20 @@
+let prevContentHeight = 0;
+
 function getTextareaRowsCount(textarea) {
     const styles = window.getComputedStyle(textarea);
     const lineHeight = parseInt(styles.lineHeight);
     const borderSize = parseInt(styles.borderTopWidth) + parseInt(styles.borderBottomWidth);
     const paddingSize = parseInt(styles.paddingTop) + parseInt(styles.paddingBottom);
     const contentHeight = textarea.scrollHeight - paddingSize - borderSize;
-    const rowsCount = Math.ceil(contentHeight / lineHeight);
+
+    let rowsCount = Math.ceil(contentHeight / lineHeight);
+    
+    if (contentHeight <= prevContentHeight) {
+        rowsCount -= (prevContentHeight - contentHeight) / lineHeight;
+    }
+    
+    prevContentHeight = contentHeight;
+
     return rowsCount;
 }
 
